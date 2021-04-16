2a7a603c7856410d97cdac3e30a20f9d11560d32
LAPTOP-46S041F2\권규현 <kyuhyun2920@naver.com>
2021-04-16 10:49:02 +0900

    점검

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import MyPost from "../components/MyPost";
 import { useSelector, useDispatch } from "react-redux"
 import { actionCreators as userActions } from "../redux/modules/post";
+import { Settings } from "@material-ui/icons";
+import Setting from  "../pages/Setting"
 
 
 //해당 user가 작성한 게시글 정보를 전부 내려주면 middleware를 통해서 reducer에 정보저장
@@ -17,40 +19,103 @@ const Mypage = () => {
       dispatch(userActions.getmyPostAX());
   },[]);
 
+  if(my_list.length===0){
+    return (<React.Fragment>
 
+    <EditProfileContainer>
+      <ImageCircle size={200}/>
+      <button>EDIT PROFILE</button>
+    </EditProfileContainer>
+    <NoPost><Text>작성한 게시물이 없습니다!</Text></NoPost>
+    </React.Fragment>
+    )
+  }else{
   return(
   <React.Fragment>
+  <EditProfileContainer/>
   <PostContainer>
- 
+  
+  
   {my_list.map((item)=> {
     return(
+      
       <MyPost key={item.id} {...item}></MyPost>
+      
     )}
   )}
   
   </PostContainer> 
   </React.Fragment>
   )
-};
+ }
+}
+
+
+const EditProfileContainer = styled.div`
+margin:auto;
+width:516px;
+height: 300px;
+
+background-color: white;
+border: 1px solid #e9ecef;
+border-radius: 10px;
+margin-top:100px;
+`
+
+
+const ImageCircle = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: var(--size);
+  background-color: black;
+  // background-image: url("${(props) => props.src}");
+  background-size: cover;
+  margin: 4px;
+  margin: 20px 0px 0px 30px;
+`;
+
 
 
 const PostContainer = styled.div`
   margin:auto;
+  width:534px;
+
+  background-color: black;
+  border: 3px solid black;
+  border-radius: 10px;
+  margin-top:180px;     
+  display:flex;
+  
+  // 줄바꿈 자동으로 되도록 + flex-item간의 간격이 줄어들도록!
+  flex-wrap: wrap;
+  align-content:flex-start;
+  padding: 10px 5px 10px 5px;
+
+  
+`;
+const NoPost = styled.div`
+  margin:auto;
   width:516px;
+  height: 150px;
 
   background-color: black;
   border: 1px solid #e9ecef;
   border-radius: 10px;
   margin-top:70px;     
   display:flex;
-  
-  // 줄바꿈 자동으로 되도록 + flex-item간의 간격이 줄어들도록!
-  flex-wrap: wrap;
-  align-content:flex-start;
-  padding-bottom: 20px;
-
-  
+  padding: 20px 9px 0px 0px;
 `;
+
+const Text = styled.div`
+   font-weight: bold;
+   font-size: 20px;
+   color: white;
+   
+   padding: 40px 0px 40px 150px;
+   
+  
+`
 
 
 
